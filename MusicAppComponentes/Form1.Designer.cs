4d4c7390a3f057c1168dfb18b49aef02bafd5615
claauudiaa <claudiacandelas2005@gmail.com>
2025-03-05 11:43:26 +0100

    Componentes usados creados por mi

---
+++ MusicAppComponentes/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MusicAppComponentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.customProgressBar1 = new Componentes.CustomProgressBar();
+            this.cButtonClose1 = new Componentes.CButtonClose();
+            this.cButton1 = new Componentes.CButton();
+            this.cButtonBack1 = new Componentes.CButtonBack();
+            this.cButtonStop1 = new Componentes.CButtonStop();
+            this.cButtonFolder1 = new Componentes.CButtonFolder();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customProgressBar1
+            // 
+            this.customProgressBar1.ChannelColor = System.Drawing.Color.Magenta;
+            this.customProgressBar1.ChannelHeight = 4;
+            this.customProgressBar1.KnobColor = System.Drawing.Color.Black;
+            this.customProgressBar1.Location = new System.Drawing.Point(297, 310);
+            this.customProgressBar1.Name = "customProgressBar1";
+            this.customProgressBar1.Size = new System.Drawing.Size(204, 23);
+            this.customProgressBar1.SliderColor = System.Drawing.Color.Magenta;
+            this.customProgressBar1.TabIndex = 0;
+            // 
+            // cButtonClose1
+            // 
+            this.cButtonClose1.BackColor = System.Drawing.Color.Magenta;
+            this.cButtonClose1.BorderColor = System.Drawing.Color.Magenta;
+            this.cButtonClose1.BorderRadius = 40;
+            this.cButtonClose1.BorderSize = 0;
+            this.cButtonClose1.FlatAppearance.BorderSize = 0;
+            this.cButtonClose1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButtonClose1.ForeColor = System.Drawing.Color.Transparent;
+            this.cButtonClose1.IconColor = System.Drawing.Color.Black;
+            this.cButtonClose1.Location = new System.Drawing.Point(297, 44);
+            this.cButtonClose1.Name = "cButtonClose1";
+            this.cButtonClose1.Size = new System.Drawing.Size(39, 40);
+            this.cButtonClose1.TabIndex = 2;
+            this.cButtonClose1.UseVisualStyleBackColor = false;
+            // 
+            // cButton1
+            // 
+            this.cButton1.ArrowColor = System.Drawing.Color.Black;
+            this.cButton1.BackColor = System.Drawing.Color.Magenta;
+            this.cButton1.BorderColor = System.Drawing.Color.Magenta;
+            this.cButton1.BorderRadius = 40;
+            this.cButton1.BorderSize = 0;
+            this.cButton1.FlatAppearance.BorderSize = 0;
+            this.cButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButton1.ForeColor = System.Drawing.Color.Transparent;
+            this.cButton1.Location = new System.Drawing.Point(297, 351);
+            this.cButton1.Name = "cButton1";
+            this.cButton1.Size = new System.Drawing.Size(38, 40);
+            this.cButton1.TabIndex = 3;
+            this.cButton1.UseVisualStyleBackColor = false;
+            // 
+            // cButtonBack1
+            // 
+            this.cButtonBack1.ArrowColor = System.Drawing.Color.Black;
+            this.cButtonBack1.BackColor = System.Drawing.Color.Magenta;
+            this.cButtonBack1.BorderColor = System.Drawing.Color.Magenta;
+            this.cButtonBack1.BorderRadius = 38;
+            this.cButtonBack1.BorderSize = 0;
+            this.cButtonBack1.FlatAppearance.BorderSize = 0;
+            this.cButtonBack1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButtonBack1.ForeColor = System.Drawing.Color.Transparent;
+            this.cButtonBack1.Location = new System.Drawing.Point(462, 351);
+            this.cButtonBack1.Name = "cButtonBack1";
+            this.cButtonBack1.Size = new System.Drawing.Size(39, 40);
+            this.cButtonBack1.TabIndex = 4;
+            this.cButtonBack1.UseVisualStyleBackColor = false;
+            // 
+            // cButtonStop1
+            // 
+            this.cButtonStop1.BackColor = System.Drawing.Color.Magenta;
+            this.cButtonStop1.BorderColor = System.Drawing.Color.Magenta;
+            this.cButtonStop1.BorderRadius = 32;
+            this.cButtonStop1.BorderSize = 0;
+            this.cButtonStop1.FlatAppearance.BorderSize = 0;
+            this.cButtonStop1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButtonStop1.ForeColor = System.Drawing.Color.Transparent;
+            this.cButtonStop1.IconColor = System.Drawing.Color.Black;
+            this.cButtonStop1.Location = new System.Drawing.Point(380, 351);
+            this.cButtonStop1.Name = "cButtonStop1";
+            this.cButtonStop1.Size = new System.Drawing.Size(38, 40);
+            this.cButtonStop1.TabIndex = 5;
+            this.cButtonStop1.UseVisualStyleBackColor = false;
+            // 
+            // cButtonFolder1
+            // 
+            this.cButtonFolder1.BackColor = System.Drawing.Color.Magenta;
+            this.cButtonFolder1.BorderColor = System.Drawing.Color.Magenta;
+            this.cButtonFolder1.BorderRadius = 40;
+            this.cButtonFolder1.BorderSize = 0;
+            this.cButtonFolder1.FlatAppearance.BorderSize = 0;
+            this.cButtonFolder1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButtonFolder1.ForeColor = System.Drawing.Color.Transparent;
+            this.cButtonFolder1.IconColor = System.Drawing.Color.Black;
+            this.cButtonFolder1.Location = new System.Drawing.Point(462, 44);
+            this.cButtonFolder1.Name = "cButtonFolder1";
+            this.cButtonFolder1.Size = new System.Drawing.Size(39, 40);
+            this.cButtonFolder1.TabIndex = 6;
+            this.cButtonFolder1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(331, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(317, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cantándote al oído";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(297, 289);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(464, 289);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(40, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "2:46";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cButtonFolder1);
+            this.Controls.Add(this.cButtonStop1);
+            this.Controls.Add(this.cButtonBack1);
+            this.Controls.Add(this.cButton1);
+            this.Controls.Add(this.cButtonClose1);
+            this.Controls.Add(this.customProgressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Componentes.CustomProgressBar customProgressBar1;
+        private Componentes.CButtonClose cButtonClose1;
+        private Componentes.CButton cButton1;
+        private Componentes.CButtonBack cButtonBack1;
+        private Componentes.CButtonStop cButtonStop1;
+        private Componentes.CButtonFolder cButtonFolder1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
